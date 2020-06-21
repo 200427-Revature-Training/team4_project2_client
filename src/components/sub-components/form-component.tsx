27f5c32f3277168ecd77d47fa09a9ef012267a27
Pongnatee Wongthaweesap <Lowmana69@gmail.com>
2020-06-21 09:34:30 -0700

    Created a subfolder for the componenets that will be placed into the event registration modal. Also added a few modules to be used with the components

---
+++ src/components/sub-components/form-component.tsx
@@ -0,0 +1,18 @@
+/*
+    Form Information 
+        - Name of Event
+        - Date(s) of Event
+        - Image Container
+        - Description of Event
+        - Event Capacity
+        - Price Selection
+        - Location of Event
+            - Address
+            - State
+            - City
+            - Zip Code
+*/
+
+import React from 'react'
+import {} from "@material-ui/core";
+import {} from '@material-ui/icons';
