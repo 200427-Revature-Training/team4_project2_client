875e5b7ebf9d8004552e0bd63ed7a64be074f3a5
Pongnatee Wongthaweesap <Lowmana69@gmail.com>
2020-06-21 11:02:10 -0700

    Added sub-components to separate out the components for easier access or for other uses in the application. Created the Host Event Component, but may need additional styling and input for better result

--- src/components/sub-components/address-component.tsx
+++ src/components/sub-components/address-component.tsx
@@ -1,3 +1,69 @@
 import React from 'react'
-import {} from "@material-ui/core";
-import {} from '@material-ui/icons';
+import { makeStyles, Theme, 
+    createStyles, TextField } from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: '25ch',
+    },
+  }),
+);
+
+export const AddressComponent: React.FC = () => {
+    
+    const classes = useStyles();
+
+    return (
+        <form className={classes.root} noValidate autoComplete="off">
+            <div>
+                <TextField
+                    id="outlined-full-width"
+                    label="Address"
+                    style={{ margin: 8 }}
+                    helperText="ex. 1234 Main Strreet"
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        shrink: true,
+                }}
+                    variant="outlined"
+                />
+                <TextField
+                    label="City"
+                    id="outlined-margin-normal"
+                    defaultValue="Default Value"
+                    className={classes.textField}
+                    helperText="ex. New Orleans"
+                    margin="normal"
+                    variant="outlined"
+                />
+                <TextField
+                    label="State"
+                    id="outlined-margin-normal"
+                    defaultValue="Default Value"
+                    className={classes.textField}
+                    helperText="ex. New York"
+                    margin="normal"
+                    variant="outlined"
+                />
+                <TextField
+                    label="Zip Code"
+                    id="outlined-margin-normal"
+                    defaultValue="Default Value"
+                    className={classes.textField}
+                    helperText="ex. 86138-1328"
+                    margin="normal"
+                    variant="outlined"
+                />
+            </div>
+        </form>
+    );
+};
+
--- src/components/sub-components/form-component.tsx
+++ src/components/sub-components/form-component.tsx
@@ -14,5 +14,62 @@
 */
 
 import React from 'react'
-import {} from "@material-ui/core";
+import { makeStyles, Theme, createStyles, TextField } from "@material-ui/core";
 import {} from '@material-ui/icons';
+import { ImageUploadButton } from './image-component';
+import { AddressComponent } from './address-component';
+import { DatePickerComponent } from './date-component';
+import { PricePickerComponent, CapacityPickerComponent } from './picker-component';
+
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }),
+);
+export const EventRegisterComponent: React.FC = () => {
+
+    const classes = useStyles();
+
+    return (
+        <form className={classes.root} noValidate autoComplete="off">
+            <div>
+                <TextField
+                        required
+                        id="outlined-full-width"
+                        label="Name of Event"
+                        style={{ margin: 8 }}
+                        helperText="ex. Pokemon Tournament"
+                        fullWidth
+                        margin="normal"
+                        InputLabelProps={{
+                            shrink: true,
+                    }}
+                        variant="outlined"
+                />
+                <DatePickerComponent />
+                <ImageUploadButton />
+                <TextField
+                        id="outlined-full-width"
+                        label="Description of Event"
+                        style={{ margin: 8 }}
+                        helperText="ex. Come and Join...."
+                        fullWidth
+                        margin="normal"
+                        InputLabelProps={{
+                            shrink: true,
+                    }}
+                        variant="outlined"
+                />
+                <AddressComponent />
+                <PricePickerComponent />
+                <CapacityPickerComponent />
+            </div>
+        </form>
+    );
+};
