31226089315571dfdcfd78c6b0c60103db0d78d0
Pongnatee Wongthaweesap <Lowmana69@gmail.com>
2020-06-23 08:12:28 -0700

    Fixing merging conflict for MyEventComponent

--- src/components/Pages/my-event.component.tsx
+++ src/components/Pages/my-event.component.tsx
@@ -1,10 +1,7 @@
 import React from "react";
-<<<<<<< HEAD
 import { UserEventComponent } from "./users-event-container-component/user-event-container.component";
 import { CreateEventModal } from "./create-event-modal.component";
-=======
-import { CreateEventModal } from "./create-event-modal.component";
->>>>>>> a9dbafbc48d2ca533f67729eb9296f47b134ba2a
+
 
 export const MyEventComponent: React.FC = () => {
   return (
