3ee86c0d2b137c3ac421182836971dabdbaa5ebb
bukadirit <bukadiri.trawally@mail.citytech.cuny.edu>
2020-06-29 22:32:17 -0400

    Changed the forum remote to fetch data from spring server. Added states to the main component necessary for sharing data between other components

--- src/remotes/forum.remote.ts
+++ src/remotes/forum.remote.ts
@@ -3,19 +3,22 @@ import { Event, Post, Comment } from "../models/Forum";
 
 export const getForumEvent = async (eventpost: string) => {
   const response = await internalAxios.get<Event[]>(
-    `/event/${eventpost}/event`
+    `/event?eventId=${eventpost}`
   );
   return response.data;
 };
 
 export const getForumPost = async (eventpost: string) => {
-  const response = await internalAxios.get<Post[]>(`/event/${eventpost}/post`);
+  const response = await internalAxios.get<Post[]>(
+    `/post?noteventTypeId=${eventpost}`
+  );
   return response.data;
 };
 
 export const getForumComment = async (eventpost: string) => {
   const response = await internalAxios.get<Comment[]>(
-    `/event/${eventpost}/comment`
+    `/comment?postId=
+    ${eventpost}`
   );
   return response.data;
 };
